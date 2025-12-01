4e15ab1398dec54f8fdadb0b26ee806a681d825b
vasirtours19 <vasirtours19@gmail.com>
2025-12-01 11:36:28 -0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -11,7 +11,8 @@ return [
         'http://localhost:8080',
         'http://192.168.1.5:8080',
         'http://127.0.0.1:8080',
-        'https://vasir.onrender.com'
+        'https://vasir.onrender.com',
+        'https://vasirsv.com'
     ],
     'allowed_origins_patterns' => [
         '/^https:\/\/.*\.railway\.app$/',
