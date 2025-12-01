653fea49154137b186222e6bd411628df85a7bf1
vasirtours19 <vasirtours19@gmail.com>
2025-12-01 12:45:41 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -26,14 +26,14 @@ export default defineConfig({
 
 
 // //Configuración del servidor de desarrollo solo para pruebas en red local
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        cors: true,
-        hmr: {
-            host: '192.168.1.5'
-        }
-    },
+    // server: {
+    //     host: '0.0.0.0',
+    //     port: 5173,
+    //     cors: true,
+    //     hmr: {
+    //         host: '192.168.1.5'
+    //     }
+    // },
 
     // Optimizaciones para build en Railway
     build: {
